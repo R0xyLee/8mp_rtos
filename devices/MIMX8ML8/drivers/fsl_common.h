b59942dc0f871ef892dfcea8f21d2df73792dd58
R0xy <lee.lockhey@gmail.com>
2026-05-09 16:30:42 +0800

    update .gitignore
    Signed-off-by: R0xy <lee.lockhey@gmail.com>

--- devices/MIMX8ML8/drivers/fsl_common.h
+++ devices/MIMX8ML8/drivers/fsl_common.h
@@ -39,6 +39,8 @@
  * Definitions
  ******************************************************************************/
 
+#define __maybe_unused                  __attribute__((__unused__))
+
 /*! @brief Construct a status code value from a group and code number. */
 #define MAKE_STATUS(group, code) ((((group)*100) + (code)))
 
